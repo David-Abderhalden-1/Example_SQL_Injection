ab9ac51098c4411b7115273df8ce99e8b9542884
unknown <david.abderhalden@hotmail.com>
2020-11-11 14:29:14 +0100

    (Added register, admin login, user_data table)

--- main.py
+++ main.py
@@ -4,31 +4,50 @@ from Example_SQL_Injection import mydata
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST', 'GET'])
-def sessions():
-
-    if request.method == 'POST':
-        session['name'] = request.form['username']
-        session['password'] = request.form['password']
-        if mydata.login_checker(session['name'], session['password']):
-            if not session['name'] or not session['password'] == 0:
-                return redirect(request.url)
-        else:
-            return render_template("index.html")
+@app.route('/index', methods=['POST', 'GET'])
+def login():
+    if 'name' in session:
+        table = (mydata.list_converter())
+        return render_template("index.html", name=escape(session['name']), password=escape(session['password']), table=table)
     else:
-        if 'name' in session:
-            return render_template("login.html", name=escape(session['name']))
-        else:
-            return render_template("index.html")
+        return render_template("login.html")
 
 
 app.secret_key = "b'\xba\x9d\xa4oBU\x8d/\x96\xe1\x04\x0e\xb3\xc6\xd5\xca\x88\r$\x10\xa3\xf2i\x9a'"
 
 
+@app.route("/login", methods=['POST', 'GET'])
+def sessions():
+    if request.method == 'POST':
+        if mydata.login_checker_injectable(request.form['username'], request.form['password']):
+            if not request.form['username'] or not request.form['password'] == 0:
+                session['name'] = request.form['username']
+                session['password'] = request.form['password']
+                return redirect(url_for('login'))
+        else:
+            return redirect(url_for('login'))
+
+
+@app.route("/register", methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        # check if data already exists.
+        if not request.form['username'] or not request.form['password'] == 0:
+            if not mydata.login_checker_safe(request.form['username'], request.form['password']):
+                session['name'] = request.form['username']
+                session['password'] = request.form['password']
+                mydata.register(escape(session['name']), escape(session['password']))
+                return redirect(url_for('login'))
+            else:
+                return redirect(url_for('login'))
+    else:
+        return redirect(url_for('login'))
+
+
 @app.route("/logout", methods=['POST', 'GET'])
 def logout():
     session.pop('name', None)
-    return redirect(url_for('sessions'))
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
--- mydata.py
+++ mydata.py
@@ -1,5 +1,5 @@
 import mysql.connector
-
+import HTML
 
 mydatabase = mysql.connector.connect(
     host="localhost",
@@ -11,18 +11,20 @@ mydatabase = mysql.connector.connect(
 my_cursor = mydatabase.cursor()
 
 
-def login_checker(username, password):
-    my_cursor.execute("SELECT * FROM user_authentication WHERE username = '" + username + "' AND password = '" + password + "';")
-    myresult = my_cursor.fetchall()
-    print(len(myresult))
-    if len(myresult) > 0:
+# Login is injectable with SQL Injection
+def login_checker_injectable(username, password):
+    my_cursor.execute("SELECT * FROM user_authentication_2 WHERE username = '" + username + "' AND password = '" + password + "';")
+    my_result = my_cursor.fetchall()
+    print(len(my_result))
+    if len(my_result) > 0:
         return True
 
 
-def login_checker_2(username, password):
-    my_cursor.execute("SELECT * FROM user_authentication")
-    myresult = my_cursor.fetchall()
-    for data in myresult:
+# Login is safe from SQL Injection
+def login_checker_safe(username, password):
+    my_cursor.execute("SELECT * FROM user_authentication_2")
+    my_result = my_cursor.fetchall()
+    for data in my_result:
         for index in data:
             if index == username:
                 pw = data[2]
@@ -30,3 +32,20 @@ def login_checker_2(username, password):
                     return True
                 else:
                     return False
+
+
+def register(username, password):
+    registry = "INSERT INTO user_authentication_2 (username, password) VALUES (%s, %s)"
+    data = (username, password)
+    my_cursor.execute(registry, data)
+    mydatabase.commit()
+
+
+def list_converter():
+    my_cursor.execute('SELECT * FROM user_authentication_2')
+    my_table = my_cursor.fetchall()
+    table_data = []
+    for row in my_table:
+        table_data.append(row)
+    html_code = HTML.table(table_data, header_row=['User_ID', 'Username', 'Password'])
+    return html_code
